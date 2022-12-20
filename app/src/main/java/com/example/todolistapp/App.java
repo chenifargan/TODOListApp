8499a78c485e5f8e3787725886ad6d2691da78a6
chenifargan <chenifargan12@gmail.com>
2022-12-20 19:43:59 +0200

    C

---
+++ app/src/main/java/com/example/todolistapp/App.java
@@ -0,0 +1,14 @@
+package com.example.todolistapp;
+
+import android.app.Application;
+
+import com.google.android.gms.ads.MobileAds;
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        MobileAds.initialize(this);
+    }
+}
