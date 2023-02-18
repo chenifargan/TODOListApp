e4a056c5eb4901640c3ce5fcd047087162bc237b
chenifargan <chenifargan12@gmail.com>
2023-02-18 16:44:19 +0200

    C

---
+++ app/src/main/java/com/example/todolistapp/Helper.java
@@ -0,0 +1,61 @@
+package com.example.todolistapp;
+
+import android.app.Activity;
+import android.os.Build;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Helper {
+    private static Helper single= null;
+    public Helper() {
+    }
+    public static Helper initHelper(){
+        if(single==null){
+            single = new Helper();
+        }
+        return single;
+    }
+
+    public static void openHtmlTextDialog(Activity activity, String fileNameInAssets) {
+        String str = "";
+        InputStream is = null;
+
+        try {
+            is = activity.getAssets().open(fileNameInAssets);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            str = new String(buffer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        MaterialAlertDialogBuilder materialAlertDialogBuilder = new MaterialAlertDialogBuilder(activity);
+        materialAlertDialogBuilder.setPositiveButton("Close", null);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            materialAlertDialogBuilder.setMessage(Html.fromHtml(str, Html.FROM_HTML_MODE_LEGACY));
+        } else {
+            materialAlertDialogBuilder.setMessage(Html.fromHtml(str));
+        }
+
+        AlertDialog al = materialAlertDialogBuilder.show();
+        TextView alertTextView = al.findViewById(android.R.id.message);
+        alertTextView.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/todolistapp/ShopActivity.java
@@ -0,0 +1,177 @@
+package com.example.todolistapp;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.google.android.material.button.MaterialButton;
+
+import top.defaults.colorpicker.ColorPickerPopup;
+
+public class ShopActivity extends AppCompatActivity {
+    private int mDefaultColor=0;
+    private ImageButton btn_choose_color;
+    private ConstraintLayout[] constraintLayout;
+    private ImageView [] images;
+    private MaterialButton btn_back;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop);
+
+        initViews();
+
+
+        btn_choose_color.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addColor();
+
+            }
+        });
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ShopActivity.this,HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+
+    private void initViews() {
+        btn_choose_color= findViewById(R.id.btn_choose_color);
+    constraintLayout = new ConstraintLayout[]{findViewById(R.id.ConstraintLayout1),findViewById(R.id.ConstraintLayout2),findViewById(R.id.ConstraintLayout3)};
+    images= new ImageView[] {findViewById(R.id.iv_shop1),findViewById(R.id.iv_shop2),findViewById(R.id.iv_shop3),findViewById(R.id.iv_shop4)};
+    btn_back = findViewById(R.id.btn_back);
+
+    }
+
+    private void addColor() {
+        AlertDialog.Builder myDialog = new AlertDialog.Builder(this);
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View myView = inflater.inflate(R.layout.input_color,null);
+        myDialog.setView(myView);
+        final AlertDialog dialog = myDialog.create();
+        dialog.setCancelable(false);
+        final Button mSetColorButton =myView.findViewById(R.id.set_color_button);
+        final Button mPickColorButton=myView.findViewById(R.id.pick_color_button);
+        final Button back =  myView.findViewById(R.id.back);
+        final View mColorPreview=myView.findViewById(R.id.preview_selected_color);
+
+           mDefaultColor=0;
+        mPickColorButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(final View v) {
+                        new ColorPickerPopup.Builder(ShopActivity.this).initialColor(
+                                        Color.RED) // set initial color
+                                // of the color
+                                // picker dialog
+                                .enableBrightness(
+                                        true) // enable color brightness
+                                // slider or not
+                                .enableAlpha(
+                                        true) // enable color alpha
+                                // changer on slider or
+                                // not
+                                .okTitle(
+                                        "Choose") // this is top right
+                                // Choose button
+                                .cancelTitle(
+                                        "Cancel") // this is top left
+                                // Cancel button which
+                                // closes the
+                                .showIndicator(
+                                        true) // this is the small box
+                                // which shows the chosen
+                                // color by user at the
+                                // bottom of the cancel
+                                // button
+                                .showValue(
+                                        true) // this is the value which
+                                // shows the selected
+                                // color hex code
+                                // the above all values can be made
+                                // false to disable them on the
+                                // color picker dialog.
+                                .build()
+                                .show(
+                                        v,
+                                        new ColorPickerPopup.ColorPickerObserver() {
+                                            @Override
+                                            public void
+                                            onColorPicked(int color) {
+                                                // set the color
+                                                // which is returned
+                                                // by the color
+                                                // picker
+                                                mDefaultColor = color;
+
+                                                // now as soon as
+                                                // the dialog closes
+                                                // set the preview
+                                                // box to returned
+                                                // color
+                                                mColorPreview.setBackgroundColor(mDefaultColor);
+                                            }
+                                        });
+                    }
+                });
+
+        // handling the Set Color button to set the selected
+        // color for the GFG text.
+        mSetColorButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // now change the value of the GFG text
+                        // as well.
+
+                        btn_choose_color.setBackgroundColor(mDefaultColor);
+                        back.setBackgroundColor(mDefaultColor);
+                        mPickColorButton.setBackgroundColor(mDefaultColor);
+                        mSetColorButton.setBackgroundColor(mDefaultColor);
+                        for (int i = 0; i <constraintLayout.length ; i++) {
+                            constraintLayout[i].setBackgroundColor(mDefaultColor);
+                        }
+                        for (int i=0;i<images.length;i++){
+                            images[i].setColorFilter(mDefaultColor);
+
+                        }
+                    }
+                });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dialog.dismiss();
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+        dialog.show();
+
+    }
+}
